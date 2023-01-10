5cfa22c6b23c5a8ee010bdecd4d74be58ecd8bb8
Alex Allen <99975765+AlexAllen247@users.noreply.github.com>
2023-01-10 11:22:30 +0100

    completed exercises 13.13-13.16

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const { Blog, User } = require("../models");
 const { tokenExtractor } = require("../middleware/tokenExtractor");
+const { Op } = require("sequelize");
 
 const blogFinder = async (req, res, next) => {
   req.blog = await Blog.findByPk(req.params.id);
@@ -8,11 +9,29 @@ const blogFinder = async (req, res, next) => {
 };
 
 router.get("/", async (req, res) => {
+  const where = {};
+  if (req.query.search) {
+    where[Op.or] = [
+      {
+        title: {
+          [Op.substring]: req.query.search,
+        },
+      },
+      {
+        author: {
+          [Op.substring]: req.query.search,
+        },
+      },
+    ];
+  }
+
   const blogs = await Blog.findAll({
     attributes: { exclude: ["userId"] },
     include: {
       model: User,
     },
+    where,
+    order: [["likes", "DESC"]],
   });
   res.json(blogs);
 });
--- index.js
+++ index.js
@@ -5,11 +5,13 @@ require("express-async-errors");
 const { PORT } = require("./util/config");
 const { connectToDatabase } = require("./util/db");
 
-const blogsRouter = require("./controllers/blogs");
+const tokenExtractor = require("./middleware/tokenExtractor");
 const errorHandler = require("./middleware/errorHandler");
+
+const blogsRouter = require("./controllers/blogs");
 const usersRouter = require("./controllers/users");
 const loginRouter = require("./controllers/login");
-const tokenExtractor = require("./middleware/tokenExtractor");
+const authorsRouter = require("./controllers/authors");
 
 app.use(express.json());
 
@@ -17,6 +19,7 @@ app.use("/api/blogs", tokenExtractor, blogsRouter);
 app.use(errorHandler);
 app.use("/api/users", usersRouter);
 app.use("/api/login", loginRouter);
+app.use("/api/authors", authorsRouter)
 
 const start = async () => {
   await connectToDatabase();
